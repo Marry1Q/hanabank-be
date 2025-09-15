008e43d9f35908a0c250b741aa6df8d7b2d461d6
kiv9908 <leebongwook0805@gmail.com>
2025-09-15 13:33:54 +0900

    Fix SwaggerConfig - Add Info and Components
    - Add Info section with title, description, and version
    - Add Components section for proper OpenAPI structure
    - This should resolve 502 error for /api-docs endpoint

--- src/main/java/com/kopo_team4/hanabank_backend/global/config/SwaggerConfig.java
+++ src/main/java/com/kopo_team4/hanabank_backend/global/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.kopo_team4.hanabank_backend.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +15,19 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(apiInfo())
                 .servers(List.of(
                         new Server()
                                 .url("https://hanabank-be-production.up.railway.app")
                                 .description("Production Server")
-                ));
+                ))
+                .components(new Components());
+    }
+    
+    private Info apiInfo() {
+        return new Info()
+                .title("하나은행 Backend API")
+                .description("하나은행 백엔드 API 문서")
+                .version("1.0.0");
     }
 } 
